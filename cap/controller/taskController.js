2e87eb2f73233897951b7e44e531ade6553e418d
Ani Emmanuel <117519788+AniSmart-86@users.noreply.github.com>
2024-11-29 18:47:34 +0100

    Update taskController.js

--- cap/controller/taskController.js
+++ cap/controller/taskController.js
@@ -12,21 +12,21 @@ const createTask = asyncHandler(async (req, res) => {
         return res.status(400).json({ success: false, message: "Title is required" });
     }
 
-    const task = await Task.create({ title, description, deadline, priority });
+    const task = await Task.create({ title, description, deadline, priority, createdBy: req.user._id, });
     res.status(201).json({ success: true, data: task });
 });
 
 // Get all tasks
 const getAllTasks = asyncHandler(async (req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find({ createdBy: req.user._id});
     res.status(200).json({ success: true, data: tasks });
 });
 
 // Get a single task by ID
 const getTaskById = asyncHandler(async (req, res) => {
-    const { id } = req.params;
+    // const { id } = req.params;
 
-    const task = await Task.findById(id);
+    const task = await Task.findById({ _id: req.params.id, createdBy: req.user._id });
     if (!task) {
         return res.status(404).json({ success: false, message: "Task not found" });
     }
